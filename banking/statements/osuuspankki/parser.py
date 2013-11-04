1d478051f8bb99aec680fc9bddfa105740fc2be4
Petri Savolainen <petri.savolainen@iki.fi>
2013-11-04 15:47:51 +0200

    consolidate parser variations; handle broken CSV (&amp;amp;) and add quote char to CSV dialect

--- banking/statements/osuuspankki/parser.py
+++ banking/statements/osuuspankki/parser.py
@@ -1,13 +1,28 @@
 from ofxstatement.parser import CsvStatementParser
 import csv
+from io import StringIO
 
 from . import TRANSACTION_TYPES
 
-SIGNATURE = "Kirjauspäivä;Arvopäivä;Määrä EUROA;Laji;Selitys;Saaja/Maksaja;\
-Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus"
+# The bank for some reason has varied the column labeling a lot.
+# By collecting first lines of known CSV exports as signatures
+# we catch any future changes to the CSV export format.
+
+SIGNATURES = (
+"Kirjauspäivä;Arvopäivä;Määrä  EUROA;\"Laji\";Selitys;\
+Saaja/Maksaja;Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;",
+"Kirjauspäivä;Arvopäivä;Määrä  ;\"Laji\";Selitys;Saaja/Maksaja;\
+Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;",
+"Kirjauspäivä;Arvopäivä;Määrä EUROA;Tapahtumalajikoodi;Selitys;\
+Saaja/Maksaja;Saajan tilinumero;Viite;Viesti;Arkistotunnus;",
+"Kirjauspäivä;Arvopäivä;Määrä EUROA;Laji;Selitys;Saaja/Maksaja;\
+Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;",
+"Kirjauspäivä;Arvopäivä;Määrä EUROA;Laji;Selitys;Saaja/Maksaja;\
+Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus",
+)
 
 class OPCsvStatementParser(CsvStatementParser):
-    "parser the corporate acct statement, from 2013 on"
+    "parser for various variations with common field semantics"
     
     mappings = {
        "date":1, "amount":2, "trntype":4, "payee":5,
@@ -16,15 +31,23 @@ class OPCsvStatementParser(CsvStatementParser):
 
     date_format = "%d.%m.%Y"
 
+    def __init__(self, fin):
+        sin=StringIO()
+        for l in fin:
+           # Some versions from 2011 have broken CSV...
+           sin.write(l.replace("&amp;amp;", "&"))
+        sin.seek(0)
+        super().__init__(sin)
+
     def split_records(self):
-        return csv.reader(self.fin, delimiter=';')
+        return csv.reader(self.fin, delimiter=';', quotechar='"')
 
     def parse_record(self, line):
         #Free Headerline
         if self.cur_record <= 1:
             return None
 
-        #Change decimalsign from , to .
+        # Change decimalsign from , to .
         line[2] = line[2].replace(',', '.')
 
         # Set transaction type
