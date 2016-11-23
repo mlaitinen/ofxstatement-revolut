94fec8ae37b85407a191b3f4cda8f465b93988ee
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-11-23 10:38:09 +0200

    add support for two broken formats used in 2012

--- banking/statements/osuuspankki/parser.py
+++ banking/statements/osuuspankki/parser.py
@@ -1,7 +1,9 @@
-from ofxstatement.parser import CsvStatementParser
 import csv
+import types
 from io import StringIO
 
+from ofxstatement.parser import CsvStatementParser
+from ofxstatement.statement import StatementLine, BankAccount
 from . import TRANSACTION_TYPES
 
 # The bank for some reason has varied the column labeling a lot.
@@ -9,6 +11,10 @@ from . import TRANSACTION_TYPES
 # we catch any future changes to the CSV export format.
 
 SIGNATURES = (
+'Kirjauspäivä;Arvopäivä;Määrä  MF NR;"Laji";Selitys;\
+Saaja/Maksaja;Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;',
+'Kirjauspäivä;Arvopäivä;Määrä  NDEAF;"Laji";Selitys;\
+Saaja/Maksaja;Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;',
 "Kirjauspäivä;Arvopäivä;Määrä  EUROA;\"Laji\";Selitys;\
 Saaja/Maksaja;Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;",
 "Kirjauspäivä;Arvopäivä;Määrä  ;\"Laji\";Selitys;Saaja/Maksaja;\
@@ -21,12 +27,23 @@ Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;",
 Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus",
 )
 
+
+class CustomStatementLine(StatementLine):
+    "don't print the check number"
+
+    def __str__(self):
+        return """
+        ID: %s, date: %s, amount: %s, payee: %s
+        memo: %s
+        """ % (self.id, self.date, self.amount, self.payee, self.memo)
+
+
 class OPCsvStatementParser(CsvStatementParser):
     "parser for various variations with common field semantics"
     
     mappings = {
        "date":1, "amount":2, "trntype":4, "payee":5,
-       "acctto":6, "refnum":7, "memo":8, "id":9
+       "bank_account_to":6, "refnum":7, "memo":8, "id":9
     }
 
     date_format = "%d.%m.%Y"
@@ -42,6 +59,12 @@ class OPCsvStatementParser(CsvStatementParser):
     def split_records(self):
         return csv.reader(self.fin, delimiter=';', quotechar='"')
 
+    def parse_value(self, value, field):
+       if field == "bank_account_to":
+          return BankAccount("", value)
+       else:
+          return super().parse_value(value, field)
+
     def parse_record(self, line):
         #Free Headerline
         if self.cur_record <= 1:
@@ -53,6 +76,12 @@ class OPCsvStatementParser(CsvStatementParser):
         # Set transaction type
         line[4] = TRANSACTION_TYPES[line[4]]
 
-        # fill statement line according to mappings
-        sl = super(OPCsvStatementParser, self).parse_record(line)
-        return sl
+        stmt_line = CustomStatementLine()
+        for field, col in self.mappings.items():
+            if col >= len(line):
+                raise ValueError("Cannot find column %s in line of %s items "
+                                 % (col, len(line)))
+            rawvalue = line[col]
+            value = self.parse_value(rawvalue, field)
+            setattr(stmt_line, field, value)
+        return stmt_line
